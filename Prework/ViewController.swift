cc9043e7418901bc3d3c12b3ba04ce290be686d5
Phi Henry Nguyen <phi.ng1114@gmail.com>
2021-01-12 18:26:02 -0600

    initial commit

---
+++ Prework/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Phi Nguyen on 1/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    
+    var backgroundColor: UIColor!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        backgroundColor = view.backgroundColor;
+    }
+
+    @IBAction func didTapButton(_ sender: Any) {
+        textLabel.textColor = UIColor.purple;
+    }
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        backgroundView.backgroundColor = UIColor.lightGray;
+    }
+    
+    @IBAction func didTapTextButton(_ sender: Any) {
+        textLabel.text = "Goodbye !";
+        textLabel.text = textField.text;
+        textField.text = "";
+        view.endEditing(true);
+    }
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        textLabel.text = "Hello from Phi!!";
+        textLabel.textColor = UIColor.black;
+        backgroundView.backgroundColor = backgroundColor;
+        
+    }
+}
+
